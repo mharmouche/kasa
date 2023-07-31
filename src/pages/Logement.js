f45bc5abe469afdf73bc60e11851d5b6158f0e70
Mohamed Harmouche <mohamed.harmouche@gmail.com>
2023-07-31 03:21:21 +0000

    logement : extract all data

--- src/pages/Logement.js
+++ src/pages/Logement.js
@@ -1,6 +1,16 @@
 import datas from "../datas/logements.json";
 
-
+function NumberList(props) {
+  const numbers = props.numbers;
+  const listItems = numbers.map((number) =>
+    <li key={number.toString()}>
+      {number}
+    </li>
+  );
+  return (
+    <ul>{listItems}</ul>
+  );
+}
 
 const Logement = () => {
   const queryParameters = new URLSearchParams(window.location.search)
@@ -16,16 +26,29 @@ const Logement = () => {
   const rating = dataLogement[0].rating
   const location = dataLogement[0].location
   const equipments = dataLogement[0].equipments
-  const tags = dataLogement.tags 
+  const tags = dataLogement[0].tags 
   return (
     <>
       <h1>Détails de votre logement </h1>
       <p>id = {id}</p> 
       <p>title = {title}</p> 
       <p>cover = {cover}</p> 
+      <p>pictures = 
+        <NumberList numbers={pictures} /></p>
       <p>description = {description}</p> 
+      
+      <p>hostname = {host.name}</p> 
+      <p>hostpic = {host.picture}</p> 
+
       <p>rating = {rating}</p> 
       <p>location = {location}</p> 
+      <p>equipments = 
+        <NumberList numbers={equipments} /></p>
+      
+      <p>
+        tags <NumberList numbers={tags} /></p>
+      
+
     </>
   );
 };

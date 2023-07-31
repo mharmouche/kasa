ca9900bf51837bd2c0a590406b186aa1dfcfa49a
Mohamed Harmouche <mohamed.harmouche@gmail.com>
2023-07-31 01:13:40 +0000

    home fixe : add key to remove warning

--- src/pages/Logement.js
+++ src/pages/Logement.js
@@ -5,8 +5,23 @@ import datas from "../datas/logements.json";
 const Logement = () => {
   const queryParameters = new URLSearchParams(window.location.search)
   const id = queryParameters.get("id")
-  
-  return <h1>Détails de votre logement, id = {id}</h1>;
+  const dataLogement = datas.filter(data => data.id === id);
+  return (
+    <>
+      <h1>Détails de votre logement </h1>
+      <p>id1 = {id}</p> 
+      <p>id2 = {dataLogement.id}</p> 
+      <p>title = {dataLogement.title}</p> 
+      <p>cover = {dataLogement.cover}</p> 
+      <p>pictures = {dataLogement.pictures}</p> 
+      <p>description = {dataLogement.description}</p> 
+      <p>host = {dataLogement.host}</p> 
+      <p>rating = {dataLogement.rating}</p> 
+      <p>location = {dataLogement.location}</p> 
+      <p>equipments = {dataLogement.equipments}</p> 
+      <p>tags = {dataLogement.tags}</p> 
+    </>
+  );
 };
 
 export default Logement;

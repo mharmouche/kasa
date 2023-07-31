165dab521d1c00695aaeff4b5da1c1a8b121f487
Mohamed Harmouche <mohamed.harmouche@gmail.com>
2023-07-31 02:00:59 +0000

    Logement : get data

--- src/pages/Logement.js
+++ src/pages/Logement.js
@@ -4,22 +4,28 @@ import datas from "../datas/logements.json";
 
 const Logement = () => {
   const queryParameters = new URLSearchParams(window.location.search)
-  const id = queryParameters.get("id")
+  const id = queryParameters.get("id").toString()
   const dataLogement = datas.filter(data => data.id === id);
+  //console.log(datas)
+  //console.log(dataLogement[0])
+  const title = dataLogement[0].title
+  const cover = dataLogement[0].cover
+  const pictures = dataLogement[0].pictures
+  const description = dataLogement[0].description
+  const host = dataLogement[0].host
+  const rating = dataLogement[0].rating
+  const location = dataLogement[0].location
+  const equipments = dataLogement[0].equipments
+  const tags = dataLogement.tags 
   return (
     <>
       <h1>Détails de votre logement </h1>
-      <p>id1 = {id}</p> 
-      <p>id2 = {dataLogement.id}</p> 
-      <p>title = {dataLogement.title}</p> 
-      <p>cover = {dataLogement.cover}</p> 
-      <p>pictures = {dataLogement.pictures}</p> 
-      <p>description = {dataLogement.description}</p> 
-      <p>host = {dataLogement.host}</p> 
-      <p>rating = {dataLogement.rating}</p> 
-      <p>location = {dataLogement.location}</p> 
-      <p>equipments = {dataLogement.equipments}</p> 
-      <p>tags = {dataLogement.tags}</p> 
+      <p>id = {id}</p> 
+      <p>title = {title}</p> 
+      <p>cover = {cover}</p> 
+      <p>description = {description}</p> 
+      <p>rating = {rating}</p> 
+      <p>location = {location}</p> 
     </>
   );
 };

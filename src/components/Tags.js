ee9dfd2e842999705125596107d174e15da7a9d9
Mohamed Harmouche <mohamed.harmouche@gmail.com>
2023-08-03 21:04:25 +0000

    Logement : color adjust

--- src/components/Tags.js
+++ src/components/Tags.js
@@ -6,9 +6,7 @@ import Tag from './Tag'
 function TagList(props) {
   const tags = props.tags;
   const listItems = tags.map((tag) =>
-    <div key={tag.toString()}>
       <Tag tagName = {tag} />
-    </div>
   );
   return (
     <div className='ks-tags'>{listItems}</div>

724f788f64ff591b410a73f45a9689ab98686724
Mohamed Harmouche <mohamed.harmouche@gmail.com>
2023-08-03 10:23:24 +0000

    init star

--- src/components/Rating.js
+++ src/components/Rating.js
@@ -1,8 +1,18 @@
+import StarActive from '../assets/star-active 1.png'
+import StarInactive from '../assets/star-inactive 1.png'
+
 
 const Rating = ({rating}) => {
+  const n = {rating};
+  for (let i = 0; i < n; i++) {
+    <img src={StarActive} alt=''   />
+  }
   return (
     <>
-        <span> Rating = {rating} /5  </span>
+        <span> Note = {rating} /5  </span>
+        
+        <img src={StarActive} alt=''   />
+        <img src={StarInactive} alt=''   />
     </>
   )
 };

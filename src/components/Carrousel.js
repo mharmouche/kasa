f65168344fd57a159b9c58e985b5a7e02a061dc5
Mohamed Harmouche <mohamed.harmouche@gmail.com>
2023-08-05 20:19:33 +0000

    init narrow carrousel

--- src/components/Carrousel.js
+++ src/components/Carrousel.js
@@ -1,11 +1,15 @@
 import '../styles/carrousel.css'
+import ArrowBack from '../assets/arrow_back_ios-24px 1.png'
+import ArrowForward from '../assets/arrow_forward_ios-24px 1.png'
 
 const Carrousel = ({pictures}) => {
     console.log("carroussel " + pictures[0])
     return (
     <>
         <div className="ks-carrousel">
-            <img src={pictures[0]} alt="" className="ks-carrousel-img"/>
+        <img src={ArrowBack} alt="" className='ks-arrow-back'/>
+        <img src={ArrowForward} alt="" className='ks-carrousel-forward'/>
+        <img src={pictures[0]} alt="" className="ks-carrousel-img"/>
         </div>
     </>);
   };

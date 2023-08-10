5c333d44ac7ce636dcf93ce547ff9194e017c7ac
Mohamed Harmouche <mohamed.harmouche@gmail.com>
2023-08-10 17:57:55 +0000

    dropdown event : test to improve

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,12 +1,19 @@
 import Rectangle from './Rectangle';
 import '../styles/dropdown.css'
 
-const Dropdown = ({title, text, direction}) => {
+import { useState } from "react";
 
+
+const Dropdown = ({title, text}) => {
+    const [direction, setDirection] = useState('up');
+    const toggleDirection = () => {
+      setDirection ( direction === 'up' ? 'down' : 'up' );
+      console.log('direction : ' , direction)
+    }
     return (
     <>
-        <div className='ks-dropdown-box'>
-            <Rectangle text = {title}  direction={direction}/>
+        <div className='ks-dropdown-box' onClick={toggleDirection} >
+            <Rectangle text = {title}  direction={direction} />
             {direction === 'up' ? <div className='ks-dropdown-details'>{text}</div> : null}
         </div>
         
--- src/pages/Propos.js
+++ src/pages/Propos.js
@@ -2,16 +2,11 @@ import Dropdown from '../components/Dropdown'
 import Datas from '../datas/about.json'
 import './css/propos.css'
 
-import { useState } from "react";
 
 
 const Propos = () => {
-  const [direction, setDirection] = useState('up');
-  const toggleDirection = () => {
-    setDirection ( direction === 'up' ? 'down' : 'up' );
-    console.log('direction : ' , direction)
-  }
-  const Dropdowns = Datas.map((Data) => <Dropdown title = {Data.title} text={Data.txt} direction="up"/>);
+
+  const Dropdowns = Datas.map((Data) => <Dropdown title = {Data.title} text={Data.txt} />);
   return (
     <>
       <div className='ks-propos'>

4b2dd4bbb51fea17f4954417e12b18d7beb6a65d
Mohamed Harmouche <mohamed.harmouche@gmail.com>
2023-08-10 03:41:05 +0000

    Propos with dropdown

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -3,11 +3,13 @@ import '../styles/dropdown.css'
 
 const Dropdown = ({title, text, direction}) => {
 
-    index = index % l;
     return (
     <>
-        <Rectangle text = {title}  direction={direction}/>
-        {direction === 'up' ? <div className='ks-details'>{text}</div> : null}
+        <div className='ks-dropdown-box'>
+            <Rectangle text = {title}  direction={direction}/>
+            {direction === 'up' ? <div className='ks-dropdown-details'>{text}</div> : null}
+        </div>
+        
     </>);
   };
   
--- src/pages/Propos.js
+++ src/pages/Propos.js
@@ -1,8 +1,21 @@
-import Rectangle from '../components/Rectangle'
+import Dropdown from '../components/Dropdown'
+import Datas from '../datas/about.json'
+import './css/propos.css'
 
 const Propos = () => {
-  return <h1>About us</h1>;
+  return (
+    <>
+      <div className='ks-propos'>
+        <Dropdown title = {Datas[0].title} text={Datas[0].txt} direction="up"/>
+        <Dropdown title = {Datas[1].title} text={Datas[1].txt} direction="up"/>
+        <Dropdown title = {Datas[2].title} text={Datas[2].txt} direction="up"/>
+        <Dropdown title = {Datas[3].title} text={Datas[3].txt} direction="up"/> 
+      </div>
+         
+    </>
+  )
+  
+  
 };
 
 export default Propos;
-

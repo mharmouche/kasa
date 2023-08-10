7ae92411c44fc9c249758b8703839c4c210d1ef5
Mohamed Harmouche <mohamed.harmouche@gmail.com>
2023-08-10 17:13:41 +0000

    Propos : boucle m:ap

--- src/pages/Propos.js
+++ src/pages/Propos.js
@@ -2,14 +2,20 @@ import Dropdown from '../components/Dropdown'
 import Datas from '../datas/about.json'
 import './css/propos.css'
 
+import { useState } from "react";
+
+
 const Propos = () => {
+  const [direction, setDirection] = useState('up');
+  const toggleDirection = () => {
+    setDirection ( direction === 'up' ? 'down' : 'up' );
+    console.log('direction : ' , direction)
+  }
+  const Dropdowns = Datas.map((Data) => <Dropdown title = {Data.title} text={Data.txt} direction="up"/>);
   return (
     <>
       <div className='ks-propos'>
-        <Dropdown title = {Datas[0].title} text={Datas[0].txt} direction="up"/>
-        <Dropdown title = {Datas[1].title} text={Datas[1].txt} direction="up"/>
-        <Dropdown title = {Datas[2].title} text={Datas[2].txt} direction="up"/>
-        <Dropdown title = {Datas[3].title} text={Datas[3].txt} direction="up"/> 
+        {Dropdowns}
       </div>
          
     </>

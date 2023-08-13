d417d60a21c9d6797442dd2acd67e74cd8a73d49
Mohamed Harmouche <mohamed.harmouche@gmail.com>
2023-08-13 19:25:53 +0000

    fixe copyright year

--- src/components/Copyright.js
+++ src/components/Copyright.js
@@ -3,7 +3,7 @@ import '../styles/copyright.css'
 const Copyright = () => {
   return (
     <>
-        <div className='ks-copyright'><span> © 2020 Kasa. All rights reserved </span></div>
+        <div className='ks-copyright'><span> © 2023 Kasa. All rights reserved </span></div>
     </>
   )
 };
